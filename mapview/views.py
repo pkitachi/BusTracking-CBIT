9348c2bc8c291191d9ee770fb3883594ce5caa60
Ameeruddin <ameeruddin9848@gmail.com>
2020-04-25 18:38:41 +0530

    cbit proj view

---
+++ mapview/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Loc
+
+def homepage(request):
+	locs=Loc.objects.all()
+
+	return render(request,'homepage.html',{"locs":locs}) 
