61588fd40df76ddcd34dc49f33abbfb73505e7bd
Pavan Kalyan <pavankalyaninugurthi@gmail.com>
2020-04-29 16:27:22 +0530

    Modified Bus Details Page

--- bustrack/mapview/urls.py
+++ bustrack/mapview/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from . import views
 
 urlpatterns = [
 	path('',views.index,name='index'),
     #/homepage/
 	path('/homepage',views.homepage,name='homepage'),
-    # /dashboard/Bus-67/
-    path('/Bus-<int:bno>', views.detail, name='detail'),
+    # /dashboard/Bus-42/
+    re_path(r'^Bus-(?P<bno>[0-9]+)', views.detail, name='detail'),
     
 ]
