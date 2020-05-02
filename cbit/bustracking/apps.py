74d6de1aa8dd8387afd0dc8d0a1004a190739992
Pavan Kalyan <pavankalyaninugurthi@gmail.com>
2020-05-02 19:30:45 +0530

    - Removed Offline Bus option - Deleted CBIT Project Folder

--- cbit/bustracking/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BustrackingConfig(AppConfig):
-    name = 'bustracking'
--- cbit/bustracking/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class bus:
-	def __init__(self,no=None,lat=None,lon=None):
-		self.no=no
-		self.lat=lat
-		self.long=lon
