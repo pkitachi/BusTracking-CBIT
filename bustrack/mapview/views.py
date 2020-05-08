a9716d98a4fe41f8e1754392735200be333dc71e
Rakshitha-Yalamanchili <46837431+Rakshitha-Yalamanchili@users.noreply.github.com>
2020-05-08 23:34:51 +0530

    clusterview added

--- bustrack/mapview/views.py
+++ bustrack/mapview/views.py
@@ -27,22 +27,26 @@ def trackhistory(request):
 		temp=date.split('-')
 		temp[0],temp[2]=temp[2],temp[0]
 		date=('-'.join(temp))
-		th=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno,'deviceTime':'2020-05-01'})
+		th=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno,'deviceTime':date})
 		track_his=th.json()
 		runHrs=th.json()[-1]['runHrs']
 		return render(request,'trackhistory.html',{'runHrs':runHrs,'buses':buses,"bno":bno,"date":date,"track_his":track_his}) 
 
+def clusterview(request):
+	cluster=track.json()
+	return render(request,'clusterview.html',{'cluster':cluster,'buses': buses})
+	
+def clusterinfo(request):
+	trk=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'})
+	return JsonResponse(trk.json(),safe=False)
+
 def detail(request, bno):
-	r = requests.post('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login',data={'username':'admin','password':'admin@123'})
-	p= r.json()['access_token']
 	td=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno})
 	curRaw={'lat':td.json()[0]['latitude'],'lon':td.json()[0]['longitude']}
 	prop={'speed':td.json()[0]['speed'],'battery':td.json()[0]['battery'],'fuel':td.json()[0]['fuel']}
 	return render(request, 'bus-detail.html', {'curRaw':curRaw, 'buses': buses,'bno':bno,'prop':prop})
 
 def info(request,bno):
-	r = requests.post('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login',data={'username':'admin','password':'admin@123'})
-	p= r.json()['access_token']
 	td=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno})
 	curRaw=td.json()[0]
 	
@@ -53,7 +57,6 @@ def alerts(request):
 	return render(request,'alerts.html',{'track':track, 'buses': buses}) 
 
 def apicall(request):
-    # urlt='http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking'
     track=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'})
     track=track.json()
     alertRes = []
