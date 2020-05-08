f3b48ee875a8757d908aac9eca5fecce4f7051c5
Pavan Kalyan <pavankalyaninugurthi@gmail.com>
2020-05-08 11:25:10 +0530

    Alerts Notification Working

--- bustrack/mapview/views.py
+++ bustrack/mapview/views.py
@@ -9,11 +9,13 @@ import requests
 # urlt='http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking'
 # busses=requests.get(url).json()
 # track=requests.get(urlt).json()
-r = requests.post('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login',data={'username':'admin','password':'admin@123'})
-p= r.json()['access_token']
-td=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/routes',headers={'Authorization':f'Bearer {p}'})
-busses=td.json()
-track=[]
+# r = requests.post('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login',data={'username':'admin','password':'admin@123'})
+# p= r.json()['access_token']
+# td=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/routes',headers={'Authorization':f'Bearer {p}'})
+# busses=td.json()
+busses=[{'Id':4,'status':'WORKING','IMEI':'120654'},{'Id':5,'status':'WORKING','IMEI':'65842'},{'Id':6,'status':'WORKING','IMEI':'763425'},{'Id':7,'status':'WORKING','IMEI':'7845642321'}]
+track=[{'IMEI':'120654','alert':None},{'IMEI':'65842','alert':None},{'IMEI':'763425','alert':None},{'IMEI':'7845642321','alert':1}]
+alertRes=[]
 
 def homepage(request):
 	locs=Loc.objects.all()
@@ -24,17 +26,17 @@ def index(request):
 	return render(request,'index.html',{"locs":locs,'busses': busses}) 	
 
 def detail(request, bno):
-    
+	
 	locs=Loc.objects.all()
 	r = requests.post('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login',data={'username':'admin','password':'admin@123'})
 	p= r.json()['access_token']
 	td=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno})
 	curRaw={'lat':td.json()[0]['latitude'],'lon':td.json()[0]['longitude']}
 	prop={'speed':td.json()[0]['speed'],'battery':td.json()[0]['battery'],'fuel':td.json()[0]['fuel']};
-    #for i in track:
-    #    if(i['Id']==int(bno)):
-    #        return render(request, 'bus-detail.html', { 'bus' : i ,'locs' : locs , 'busses': busses})
-    # raise Http404("Bus does not exist")
+	#for i in track:
+	#    if(i['Id']==int(bno)):
+	#        return render(request, 'bus-detail.html', { 'bus' : i ,'locs' : locs , 'busses': busses})
+	# raise Http404("Bus does not exist")
 	return render(request, 'bus-detail.html', {'locs' : locs ,'curRaw':curRaw, 'busses': busses,'bno':bno,'prop':prop})
 
 def info(request,bno):
@@ -52,5 +54,24 @@ def alerts(request):
 
 def apicall(request):
     urlt='http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking'
-    track=requests.get(urlt).json()
-    return HttpResponse(track)
+    track=[{'IMEI':'120654','alert':1},{'IMEI':'65842','alert':None},{'IMEI':'763425','alert':None},{'IMEI':'7845642321','alert':1}]
+    flag=0
+    for i in track:
+        if i['alert']==1:
+            if len(alertRes)==0:
+                dictr = dict()
+                dictr['IMEI'] = i['IMEI']
+                dictr['new'] = 1
+                alertRes.append(dictr)
+            else:
+                for j in alertRes:
+                    if i['IMEI'] == j['IMEI']:
+                        j['new'] = 0
+                        flag=1
+                        break
+                if flag==0:
+                    dictr = dict()
+                    dictr['IMEI'] = i['IMEI']
+                    dictr['new'] = 1
+                    alertRes.append(dictr)
+    return JsonResponse(alertRes,safe=False)
