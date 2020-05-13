d0614bdb3246b4e9ff7aff8d73221b5520b2a234
Rakshitha-Yalamanchili <46837431+Rakshitha-Yalamanchili@users.noreply.github.com>
2020-05-13 11:50:33 +0530

    geofence cbit map fixed

--- bustrack/mapview/views.py
+++ bustrack/mapview/views.py
@@ -69,9 +69,12 @@ def trackhistory(request):
 		date=('-'.join(temp))
 		th=requests.get('http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/tracking',headers={'Authorization':f'Bearer {p}'},data={'routeId':bno,'deviceTime':date})
 		track_his=th.json()
-		runHrs=th.json()[-1]['runHrs']
-		dname=th.json()[0]['driverName']
-		dphone=th.json()[0]['driverPhone']
+		try:
+			runHrs=th.json()[-1]['runHrs']
+			dname=th.json()[0]['driverName']
+			dphone=th.json()[0]['driverPhone']
+		except IndexError:
+			return render(request,'indexerror.html')
 	return render(request,'trackhistory.html',{'runHrs':runHrs,'buses':buses,"bno":bno,"date":date,"track_his":track_his,"dname":dname,"dphone":dphone})
 
 def replaytracking(request):
